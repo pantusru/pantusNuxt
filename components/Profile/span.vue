4144c9a8d113d5007dc4e35f69726cf579330ec3
Максим Семенов <semenov@pantus.local>
2020-09-15 14:02:40 +0400

    add

---
+++ components/Profile/span.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-row class="align-items-center mb-3">
+        <b-col cols="2" class="mr-4">{{name}}</b-col>
+        <b-col cols="5" class="form-control form-control-sm">{{value}}</b-col>
+    </b-row>
+</template>
+
+<script>
+export default {
+    props:{
+        name:{},
+        value:{},
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/profile/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <b-container>
+        <b-row>
+            <b-col cols="3">
+                <div class="mb-5">
+                    <div class="mb-2">Максим Семенов</div>
+                    <div>m.semenov@pantus.ru</div>
+                </div>
+                 <div class="d-flex flex-column">
+                    <nuxt-link to="">Личный кабинет</nuxt-link>
+                    <nuxt-link to="">История заказов</nuxt-link>
+                    <nuxt-link to="">Избранные товары</nuxt-link>
+                 </div>
+            </b-col>
+            <b-col>
+                <b-form>
+                    <ElementForm :name="'Логин (e-mail):'"  :value="'m.semenov@pantus.ru'" />
+                    <ElementForm :name="'Имя:'"  :value="'Максим'" />
+                    <ElementForm :name="'Фамилия:'"  :value="'Семенов'" />
+                </b-form>
+            </b-col>
+        </b-row>
+    </b-container>
+</template>
+
+<script>
+import ElementForm from "@/components/Profile/span"
+export default {
+    components:{
+        ElementForm,
+    }
+}
+</script>
+
+<style>
+
+</style>

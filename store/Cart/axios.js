5423a14d5b78915488212640c53dc9b1dfbe0c43
Максим Семенов <semenov@pantus.local>
2020-09-16 15:40:33 +0400

    add

--- store/Cart/axios.js
+++ store/Cart/axios.js
@@ -12,10 +12,9 @@ export const actions = {
                         normalized: "6534-ru"
                     },
                     ProductCardImage:{
-                        url: "http://www.pantus.ru/upload/iblock/042/10034697ASP%20-%20Прокладка%20бензонасоса%20ВАЗ%2021082%20силикон%20синий%20-%20A-Sport.jpg"
+                        url: "http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"
                     },
                     album:[
-                        {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
                         {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
                     ],
                 },
@@ -25,7 +24,33 @@ export const actions = {
                     },
                     quantity: 411
                 }
-            }
+            },
+            {
+                id:19,
+                kolvo:4,
+                ProductCard:{
+                    id: 30,
+                    name: "Товар1",
+                    sku:{
+                        original: "6534-ку",
+                        normalized: "6534-ru"
+                    },
+                    ProductCardImage:{
+                        url: "http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"
+                    },
+                    album:[
+                        {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
+                    ],
+                },
+                ProductOffer:{
+                    prices:{
+                        wholesale: "356"
+                    },
+                    quantity: 411
+                }
+            },
+            
+            
         ];
     return product;
     }  
--- store/Products/axios.js
+++ store/Products/axios.js
@@ -67,10 +67,9 @@ export const actions = {
                         normalized: "6534-ru"
                     },
                     ProductCardImage:{
-                        url: "http://www.pantus.ru/upload/iblock/042/10034697ASP%20-%20Прокладка%20бензонасоса%20ВАЗ%2021082%20силикон%20синий%20-%20A-Sport.jpg"
+                        url: "http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"
                     },
                     album:[
-                        {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
                         {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
                     ],
                     ProductCardOem:[

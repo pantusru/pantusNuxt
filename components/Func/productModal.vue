90adb139071b5bc39c5d37f21231923f390e0c11
Максим Семенов <semenov@pantus.local>
2020-09-22 15:33:34 +0400

    echofunc components check

---
+++ components/Func/productModal.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+    <slot></slot>
+    <ModalImg :dataset="dataset"/>
+    <ModalBuy :ProductCard="ProductCard" :productOffer="productOffer"  />
+    </div>
+</template>
+
+<script>
+import ProductModal from "@/mixins/GetModal/product_all"
+import ModalImg from  "@/components/Modal/ProductImg"
+import ModalBuy from  "@/components/Modal/buyProduct"
+export default {
+    mixins:[ProductModal],
+    components: {
+        ModalImg,
+        ModalBuy,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- components/Products/Popular/index.vue
+++ components/Products/Popular/index.vue
@@ -31,7 +31,6 @@ export default {
         PopularProduct,
         ModalImg,
         ModalBuy,
-
     },
 }
 </script>

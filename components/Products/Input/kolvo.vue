217586fb85c95a6aeb4edafb0576077701cd6a9d
Максим Семенов <semenov@pantus.local>
2020-09-25 13:32:49 +0400

    echogo-go-go

--- components/Products/Input/kolvo.vue
+++ components/Products/Input/kolvo.vue
@@ -1,7 +1,7 @@
 <template>
     <b-form class="d-flex p-0 col-3 align-items-center">
         <b-icon-dash class="cursor-pointer" @click="dash"></b-icon-dash>
-        <b-input  :class="AddClassInput" class="ml-2 mr-2 text-center" v-model="kolvo"></b-input>
+        <b-input  :class="AddClassInput"  v-mask="'#####'" class="ml-2 mr-2 text-center" v-model="kolvo"></b-input>
         <b-icon-plus class="cursor-pointer" @click="plus"></b-icon-plus>
     </b-form>
 </template>
--- store/Cart/axios.js
+++ store/Cart/axios.js
@@ -1,66 +1,66 @@
 export const actions = { 
     async _CartProduct(){
         let  product = [
-            // {
-            //     kolvo:4,
-            //     ProductCard:{
-            //         id: 30,
-            //         name: "Товар1",
-            //         brand:{
-            //             id: 5,
-            //             name: "A-Sport"
-            //         },
-            //         sku:{
-            //             original: "6534-ку",
-            //             normalized: "6534-ru"
-            //         },
-            //         ProductCardImage:{
-            //             url: "http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"
-            //         },
-            //         album:[
-            //             {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
-            //         ],
-            //     },
-            //     ProductOffer:{
-            //         id: 3,
-            //         prices:356,
-            //         quantity: 411,
-            //         supplier:{
-            //             name: "Пантус",
-            //             deliveryDelay: "Сегодня"
-            //         }
-            //     }
-            // },
-            // {
-            //     kolvo:7,
-            //     ProductCard:{
-            //         id: 30,
-            //         name: "Товар1",
-            //         sku:{
-            //             original: "6534-ку",
-            //             normalized: "6534-ru"
-            //         },
-            //         brand:{
-            //             id: 5,
-            //             name: "A-Sport"
-            //         },
-            //         ProductCardImage:{
-            //             url: "http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"
-            //         },
-            //         album:[
-            //             {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
-            //         ],
-            //     },
-            //     ProductOffer:{
-            //         id: 13,
-            //         prices:356,
-            //         quantity: 411,
-            //         supplier:{
-            //             name: "Пумма",
-            //             deliveryDelay: "Завтра"
-            //         }
-            //     }
-            // },
+            {
+                kolvo:4,
+                ProductCard:{
+                    id: 30,
+                    name: "Товар1",
+                    brand:{
+                        id: 5,
+                        name: "A-Sport"
+                    },
+                    sku:{
+                        original: "6534-ку",
+                        normalized: "6534-ru"
+                    },
+                    ProductCardImage:{
+                        url: "http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"
+                    },
+                    album:[
+                        {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
+                    ],
+                },
+                ProductOffer:{
+                    id: 3,
+                    prices:356,
+                    quantity: 411,
+                    supplier:{
+                        name: "Пантус",
+                        deliveryDelay: "Сегодня"
+                    }
+                }
+            },
+            {
+                kolvo:7,
+                ProductCard:{
+                    id: 30,
+                    name: "Товар1",
+                    sku:{
+                        original: "6534-ку",
+                        normalized: "6534-ru"
+                    },
+                    brand:{
+                        id: 5,
+                        name: "A-Sport"
+                    },
+                    ProductCardImage:{
+                        url: "http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"
+                    },
+                    album:[
+                        {url:"http://www.pantus.ru/upload/iblock/eca/00034867ALT%20-%20Патрубки%20печки%20ВАЗ%201118%20-%20Ассоциация%20Балаковских%20Производителей%20Автозапчастей.jpg"},
+                    ],
+                },
+                ProductOffer:{
+                    id: 13,
+                    prices:356,
+                    quantity: 411,
+                    supplier:{
+                        name: "Пумма",
+                        deliveryDelay: "Завтра"
+                    }
+                }
+            },
             
             
         ];

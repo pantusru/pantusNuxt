f66422795b41b16076d6060c99fd0862a8e12afb
Максим Семенов <semenov@pantus.local>
2020-09-28 16:45:41 +0400

    echobrand fix

--- pages/brands/index.vue
+++ pages/brands/index.vue
@@ -8,44 +8,27 @@
         />
         <h1>Бренды</h1>
         <b-row class="mt-5">
-            <VirtualList
-                ref="scroll"
-                class="h-100"
-                :keeps="20"
-                :data-key="'id'"
-                :data-sources="SearchElem"
-                :data-component="NameComponent">  
-            </VirtualList>
+            <Brand v-for="source in SearchElem" :key="source.id" :source="source" />
         </b-row>
     </b-container>
 </template>
 
 <script>
-import VirtualList from 'vue-virtual-scroll-list'
-import ChecboxBrand from '@/components/Search/PanelBrand/Chexbox/index' 
 import Vinput from "@/components/Search/PanelBrand/input/index"
 import Brand from "@/components/Catalog/Brand/full"
 export default {
-    watch:{
-        SearchElem(){
-            this.$refs.scroll.scrollToOffset(0);
-        }
-    },
     async fetch({store, getters, commit}){
         await store.dispatch("Brand/BrandAll/_Brands");
     },
     data() {
         return {
-            NameComponent: Brand,
             GetName: "Brand/BrandAll/GetBrand",
             SearchElem: this.$store.getters["Brand/BrandAll/GetBrand"]
         }
     },
     components:{
         Vinput,
-        Brand,
-        ChecboxBrand,
-        VirtualList
+        Brand
     },
      methods:{ // получение результата от компонента поиска
         ValueSet(data){

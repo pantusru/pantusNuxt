cadbc72efebb65b4c8b3990cb7f90d7e7169f722
Max Semenov <m.semenov@pantus.ru>
2020-10-27 15:04:27 +0400

    git not

--- components/Profile/button.vue
+++ components/Profile/button.vue
@@ -1,46 +1,74 @@
 <template>
-  <b-button @click="SetDataUser">Изменить</b-button>
+  <div>
+    <b-button @click="SetDataUser">Изменить</b-button>
+    <b-alert
+      dismissible
+      class="w-25 mt-3"
+      @dismiss-count-down="countDownChanged"
+      :show="dismissCountDown"
+      
+      >Изменение прошли успешно</b-alert
+    >
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      /**
+       * @private Время жизни alert
+       */
+      dismissSecs: 5,
+      /**
+       * @private Переменная которая отсчитывает время когда alert изображается
+       */
+      dismissCountDown: 0,
+    }
+  },
   inject: ["$v"],
   methods: {
-    SetDataUser() {
+    /**
+     * @param dismissCountDown - Время которой alert осталось жить
+     * @function countDownChanged - Функция которая  вычитает время жизни alert
+     */
+     countDownChanged(dismissCountDown) {
+        this.dismissCountDown = dismissCountDown
+      },
+    async SetDataUser() {
       // Временное решение
       this.$v.$touch();
-      if (this.$v.Form.$error == false) { // Нет ошибок первой валидации
-        // ВРЕМЕННОЕ РЕШЕНИЕ БАН!
-        this.$store.commit("User/SetFull", {
-          name: "login",
-          value: this.$v.Form.$model.login,
-        });
-        this.$store.commit("User/SetFull", {
-          name: "name",
-          value: this.$v.Form.$model.name,
-        });
-        this.$store.commit("User/SetFull", {
-          name: "surname",
-          value: this.$v.Form.$model.surname,
-        });
-        this.$store.commit("User/SetFull", {
-          name: "telephone",
-          value: this.$v.Form.$model.telephone,
-        });
-        this.$store.commit("User/SetFull", {
-          name: "patronymic",
-          value: this.$v.Form.$model.patronymic,
-        });
-         // ВРЕМЕННОЕ РЕШЕНИЕ БАН!
-        
-        
-        if (false) {
-            this.$v.Form["password"].$model = "";
-            this.$v.Form["password2"].$model = "";
-          // Пароли с БД совпали с введеным паролем
+      // this.$v.Form.password.apiCheck = await this.ResApi()
+      console.log( this.$v.Form.password);
+      if (this.$v.Form.$error == false) {
+        // Нет ошибок первой валидации
+          // Проверка данных с API
+          // ВРЕМЕННОЕ РЕШЕНИЕ БАН!
+          this.$store.commit("User/SetFull", {
+            name: "login",
+            value: this.$v.Form.$model.login,
+          });
+          this.$store.commit("User/SetFull", {
+            name: "name",
+            value: this.$v.Form.$model.name,
+          });
+          this.$store.commit("User/SetFull", {
+            name: "surname",
+            value: this.$v.Form.$model.surname,
+          });
+          this.$store.commit("User/SetFull", {
+            name: "telephone",
+            value: this.$v.Form.$model.telephone,
+          });
+          this.$store.commit("User/SetFull", {
+            name: "patronymic",
+            value: this.$v.Form.$model.patronymic,
+          });
+          // ВРЕМЕННОЕ РЕШЕНИЕ БАН!
+          this.$v.Form["password"].$model = "";
+          this.$v.Form["password2"].$model = "";
           this.$v.Form.$reset();
-        } else { // Введен не правильный пароль
-        }
+          this.dismissCountDown = this.dismissSecs
       }
     },
   },

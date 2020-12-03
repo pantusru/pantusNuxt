942440d1a51b03c4ff098f2acba7e43f2458c53b
Max Semenov <m.semenov@pantus.ru>
2020-12-03 14:19:31 +0400

    error fix

--- components/header/header-nav/mobule/nav-mobile-index.vue
+++ components/header/header-nav/mobule/nav-mobile-index.vue
@@ -1,29 +1,23 @@
 <template>
-  <base-nav-mobule v-if="NavMobile === 'main'">
-    <base-nav-mobule-link
-      :value="dataset.value"
-      :component="dataset.components"
-      :nav="dataset.data"
-      v-for="dataset in nav"
-      :key="dataset.id"
-    />
-    <div class="px-15px">
-      <b class="nav-mobile-link-tfl d-block">
-        <a href="tel:88005558721" class="text-515151"> 8 (800) 555-87-21 </a>
-      </b>
-      <span class="nav-mobile-text-tfl"> Звонок по РФ - бесплатный </span>
-    </div>
+  <base-nav-mobule v-if="NavMobile === 'main'" :link="nav">
+    <template #footer>
+      <div class="px-15px">
+        <b class="nav-mobile-link-tfl d-block">
+          <a href="tel:88005558721" class="text-515151"> 8 (800) 555-87-21 </a>
+        </b>
+        <span class="nav-mobile-text-tfl"> Звонок по РФ - бесплатный </span>
+      </div>
+    </template>
   </base-nav-mobule>
 </template>
 
 <script>
 import mixinsNav from "@/mixins/nav-mobule/index";
 import BaseNavMobule from "@/components/base/nav-mobule/base-nav-mobule";
-import BaseNavMobuleLink from "@/components/base/nav-mobule/base-nav-mobule-link";
 
 export default {
   name: "nav-mobile",
-  components: { BaseNavMobuleLink, BaseNavMobule },
+  components: { BaseNavMobule },
   mixins: [mixinsNav],
   data() {
     return {

072ad91c5a626fb05ad5378b02caeec1a176db4d
amartynov-pantus <WAcuSUQSe56HsjmZ>
2021-01-21 09:25:05 +0400

    WWW-95 add /pay static page

---
+++ pages/pay/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-container class="pay-main-block">
+    init
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "index",
+};
+</script>
+
+<style scoped>
+
+</style>

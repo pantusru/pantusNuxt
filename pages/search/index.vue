63b21308a98151ef9b54dc9aae1f1ca2e2b10242
Max Semenov <m.semenov@pantus.ru>
2021-04-06 12:10:40 +0400

    check async filter

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -13,23 +13,25 @@
       <!-- Модалки -->
       <div class="container">
         <!-- Фильтры применяемости -->
-        <FilterApplicabilities />
+        <FilterApplicabilities v-if="getFilter" />
         <b-row class="pr-lg-3">
           <b-col cols="12" lg="3" class="mb-lg-0 mb-4">
             <!-- Общие Фильтры -->
-            <FilterForm class="block-shadow "/>
+            <FilterForm v-if="getFilter" class="block-shadow" />
           </b-col>
           <b-col lg="9" class="block-shadow border block-radius">
-            <div class="d-flex result-title justify-content-between reset-title mb-1">
+            <div
+              class="d-flex result-title justify-content-between reset-title mb-1"
+            >
               <get-count-res class="d-block" />
 
               <div class="text-right d-flex align-items-center">
                 <button-reply-show />
               </div>
             </div>
-            <hr class="my-0 mb-3">
+            <hr class="my-0 mb-3" >
             <!-- Метки какие бренды и категории выбраны  -->
-            <MetkaFilter />
+            <MetkaFilter v-if="getFilter" />
 
             <!-- Выбран вид не таблица () => показываем table-head -->
             <b-table-simple
@@ -42,7 +44,7 @@
               />
             </b-table-simple>
             <!-- Для ПК ВЕРСИИ ()=> Выбор вида товара -->
-            <div class="d-none d-lg-block ">
+            <div class="d-none d-lg-block">
               <components
                 :is="componentsName"
                 v-if="Products.length !== 0"
@@ -50,7 +52,7 @@
               />
             </div>
             <!-- Для Мобильных ()=>  вид товара блочный -->
-            <div class="d-block d-lg-none ">
+            <div class="d-block d-lg-none">
               <components
                 :is="'productBlog'"
                 v-if="Products.length !== 0"
@@ -73,13 +75,10 @@
 <script>
 import ModalImg from "@/components/modal/product-img";
 import ModalBuy from "@/components/modal/buy-product";
-import FilterApplicabilities from "@/components/forms/filter-applicabilities";
 // import PanelVid from "@/components/search/product-panel/product-thead-get";
-import FilterForm from "@/components/forms/filter-products";
 import TableProduct from "@/components/table/product/table-product-get";
 import productBlog from "@/components/func/product-blogs-get";
 import productRow from "@/components/func/product-rows-get";
-import MetkaFilter from "@/components/metka/filter/catalog-metka-get";
 import ResetFilter from "@/mixins/reset-filter/index";
 import CheckQueryFilter from "@/mixins/check-query-filter/index";
 import SubmitFilter from "@/mixins/search-submit/index";
@@ -96,17 +95,23 @@ export default {
     BasePagination,
     ButtonReplyShow,
     Share,
-    FilterForm,
+    FilterForm: () => import("@/components/forms/filter-products"),
     TableProduct,
     productBlog,
     productRow,
     PanelVid,
-    FilterApplicabilities,
-    MetkaFilter,
+    FilterApplicabilities: () =>
+      import("@/components/forms/filter-applicabilities"),
+    MetkaFilter: () => import("@/components/metka/filter/catalog-metka-get"),
     ModalImg,
     ModalBuy,
   },
   mixins: [ResetFilter, CheckQueryFilter, SubmitFilter],
+  data() {
+    return {
+      getFilter: false,
+    };
+  },
   async fetch({ query, store }) {
     //   ПРОВЕРКА QUERY
     if (query !== undefined) {
@@ -134,7 +139,7 @@ export default {
         }
       }
       if (query.filter_brands !== undefined) {
-        await  store.dispatch("Brand/BrandAll/_Brands");
+        await store.dispatch("Brand/BrandAll/_Brands");
         // ПРОВЕРКА БРЕНДА
         let brand = query.filter_brands.split(",");
         brand = Array.from(new Set(brand));
@@ -162,7 +167,7 @@ export default {
       }
       if (query.filter_categories !== undefined) {
         // ПРОВЕРКА КАТЕГОРИИ
-        await store.dispatch("Categories/CategoriesAll/_Categories")
+        await store.dispatch("Categories/CategoriesAll/_Categories");
         const ids = query.filter_categories.split(",");
         await store.dispatch("Catalog/All/_AllChexboxTrue", {
           data: store.getters["Categories/CategoriesAll/GetCategories"],
@@ -181,7 +186,9 @@ export default {
       }
       if (query.filter_applicabilities !== undefined) {
         // ПРОВЕРКА ПРИМИНИМОСТИ
-        await  store.dispatch("Applicabilities/ApplicabilitiessAll/_Applicabilitiess"); // применяемости
+        await store.dispatch(
+          "Applicabilities/ApplicabilitiessAll/_Applicabilitiess"
+        ); // применяемости
         const ids = query.filter_applicabilities.split(",");
         await store.dispatch("Applicabilities/PanelUrl/SetId_Url", {
           data:
@@ -306,23 +313,27 @@ export default {
       this.$store.getters["Categories/CategoriesAll/GetCategories"]
     );
   },
+  mounted() {
+    console.log("1");
+    this.getFilter = true;
+  },
 };
 </script>
-<style >
-.spinner-border{
+<style>
+.spinner-border {
   position: absolute;
   left: 48%;
   top: 40vh;
 }
 
-.block-shadow{
+.block-shadow {
   box-shadow: rgba(99, 99, 99, 0.2) 0 2px 8px 0;
 }
-.block-radius{
+.block-radius {
   border-radius: 6px;
 }
 
-.result-title{
+.result-title {
   height: 47px;
 }
 </style>

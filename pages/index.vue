504d947214398761d6714073f64ccf87dcbaecd9
Max Semenov <m.semenov@pantus.ru>
2021-05-12 15:59:37 +0400

    secton-5

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
     <section2 />
     <section3 />
     <section4 :brands="getCarousel" />
+    <section5 />
   </div>
 </template>
 
@@ -14,10 +15,11 @@ import Section2 from '~/components/home/section/section-2.vue'
 import Section3 from '~/components/home/section/section-3.vue'
 import { BrandCarousel } from '~/composition/brand/brand-carousel'
 import Section4 from '~/components/home/section/section-4.vue'
+import Section5 from "~/components/home/section/section-5.vue";
 
 export default Vue.extend({
   name: 'PageIndex',
-  components: { Section4, Section3, Section2, Section1 },
+  components: { Section5, Section4, Section3, Section2, Section1 },
   setup() {
     return { ...BrandCarousel() }
   },

3f61e460b427a1b94e9f1a3937ad123d573e9cda
Max Semenov <m.semenov@pantus.ru>
2021-05-19 15:35:36 +0400

    add page-404

--- pages/_.vue
+++ pages/_.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="container">
-    <div>404 Ошибка</div>
+    <div class="static">
+      <h1>404 Ошибка</h1>
+      <h2>
+        Страница не найдена. Вероятно она была удалена, а возможно она и не
+        существовала
+      </h2>
+      <p class="h2">
+        Попробуйте найти нужные запчасти с помощью поиска в верхней части
+        страницы
+      </p>
+      <p>
+        Вы можете перейти на
+        <nuxt-link to="/">главную страницу </nuxt-link>
+        или найти нужную информацию в одном из этих разделов:
+      </p>
+      <div class="flex-column">
+        <nuxt-link to="/brands">Бренды запчастей </nuxt-link>
+        <nuxt-link to="/carbrands">Марки автомобилей </nuxt-link>
+        <nuxt-link to="/categories">Категории запчастей </nuxt-link>
+        <nuxt-link to="">Каталог оригинальных запчастей </nuxt-link>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,3 +35,6 @@ export default {
   },
 }
 </script>
+<style lang="sass">
+@import "assets/sass/static/static"
+</style>
